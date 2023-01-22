78a547ac49daafdc4a80c0a9cbda5440e3821ed3
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-01-22 17:24:56 +0330

    creating getter & setter

--- lib/main_provider.dart
+++ lib/main_provider.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+
+class MainProvider extends ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  set counter(int value) {
+    if (value != _counter) {
+      _counter = value;
+      notifyListeners();
+    }
+  }
+}
